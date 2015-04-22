7e4c16c00dd00f7996d802c6da2c31f384df260e
Oriol Vilanova <oriol.vilanova@gmail.com>
2015-04-22 18:50:32 +0200

    Replaced genconfig.py and setup.dat

--- genConfig.py
+++ genConfig.py
@@ -1,77 +1,87 @@
 #!/usr/bin/python
 
-''' Script that generates a cubic box full of spheres of radius R0 in a 
-cubic lattice with a centered sphere of radius RNP '''
+''' Script that generates a cubic box full of spheres in a cubic lattice with a 
+centered sphere of radius R_NP '''
 
 import math
 import numpy as np
 import string
 import random
-
 import argparse
 
 ''' FileName '''
 fileName = 'config/init.dat'
 
-''' Fraction of each protein From less to most abundant '''
-P1 = 0.1
-P2 = 0.5
-P3 = 0.5
-
 parser = argparse.ArgumentParser()
 
 ''' General parameters, temperature in [kb*T] and lengths in [nm] '''
 parser.add_argument("-kT",       type=float, help="Temperature",              default=1.00)
-parser.add_argument("-N",        type=int,   help="Number of particles",      default=256*10)
-parser.add_argument("-L",        type=float, help="Lateral size of the box",  default=600.) 
+parser.add_argument("-N",        type=int,   help="Number of particles",      default=256*4)
+parser.add_argument("-L",        type=float, help="Lateral size of the box",  default=375.) 
 parser.add_argument("-RNP",      type=float, help="Radius of the NP",         default=50.) # L > 4*RNP
 parser.add_argument("-C_PBS",    type=float, help="Concentration of PBS in M", default=0.010)
 
-# Rh_HSA = 2.7
-# Rh_Tf  = 3.72
-# Rh_Fib = 
+''' Density of the NP [g/cm3] '''
+# Silica: 2.65 g/cm3 /// Polystyrene: 1.00 g/cm3
+parser.add_argument("-rho_NP", type=float, help="Density of the NP",          default=2.650)
+
+''' Reference starting concentrations [mg/ml] '''
+# C_HSAwSiO2(half-coverage) = 0.175
+# C_TfwSiO2(half-coverage)  = 0.055
+# C_FibwSiO2(half-coverage) = ???
+parser.add_argument("-C_NP",    type=float, help="Conc. of NPs",              default=0.001)
+parser.add_argument("-C_Prot1", type=float, help="Conc. of Protein 1",        default=0.10)
+parser.add_argument("-C_Prot2", type=float, help="Conc. of Protein 2",        default=0.10)
+parser.add_argument("-C_Prot3", type=float, help="Conc. of Protein 3",        default=0.10)
+
+''' Number of excess proteins that may be adsorbed to the NP ''' 
+parser.add_argument("-Nmin_Prot1", type=float, help="Min Num. Protein 1",     default=100)
+parser.add_argument("-Nmin_Prot2", type=float, help="Min Num. Protein 2",     default=300)
+parser.add_argument("-Nmin_Prot3", type=float, help="Min Num. Protein 3",     default=500)
 
 ''' Hard-core radius of the proteins in [nm] '''
+# Rh_HSA = 2.7
+# Rh_Tf  = 3.72
+# Rh_Fib = 6.6
 parser.add_argument("-R1",       type=float, help="Hard Radius of prot 1",    default=6.60) # Fib
 parser.add_argument("-R2",       type=float, help="Hard Radius of prot 2",    default=3.72) # Tf
 parser.add_argument("-R3",       type=float, help="Hard Radius of prot 3",    default=2.70) # HSA
 
+''' Soft-core radius of the proteins in [nm] '''
 # Rs_HSA = 3.6
 # Rs_Tf  = 3.72
-# Rs_Fib = 11.
-
-''' Soft-core radius of the proteins in [nm] '''
+# Rs_Fib = 8.5
 parser.add_argument("-Rs1",      type=float, help="Soft Radius of prot 1",    default=8.50)
 parser.add_argument("-Rs2",      type=float, help="Soft Radius of prot 2",    default=3.72)
 parser.add_argument("-Rs3",      type=float, help="Soft Radius of prot 3",    default=3.60)
 
+''' Masses of the proteins in [kDa] '''
 # M_HSA = 67
 # M_Tf  = 80
 # M_Fib = 340
-
-''' Masses of the proteins in [kDa] '''
 parser.add_argument("-M1",       type=float, help="Mass of prot 1",           default=340.00) 
 parser.add_argument("-M2",       type=float, help="Mass of prot 2",           default=80.00)
 parser.add_argument("-M3",       type=float, help="Mass of prot 3",           default=67.00)
 
-# EpsNP_HSAwSiO2 = 10.8
-# EpsNP_TfwSiO2  = 8.9
-# EpsNP_FibwSiO2 = 20.
-
 ''' Sruface affinity of each protein in [Kb*T] ''' 
-parser.add_argument("-EpsNP1", type=float, help="Int. energy NP-prot 1", default=8.0) 
-parser.add_argument("-EpsNP2", type=float, help="Int. energy NP-prot 2", default=8.3) 
+# EpsNP_HSAwSiO2 = 9.7
+# EpsNP_TfwSiO2  = 8.3
+# EpsNP_FibwSiO2 = 8.
+parser.add_argument("-EpsNP1", type=float, help="Int. energy NP-prot 1", default=9.0) 
+parser.add_argument("-EpsNP2", type=float, help="Int. energy NP-prot 2", default=15.) 
 parser.add_argument("-EpsNP3", type=float, help="Int. energy NP-prot 3", default=9.7)
 
-# > 0 for multilayer adsorption
 ''' Protein-protein interaction in the surface in [Kb*T] '''
-parser.add_argument("-EpsProt1", type=float, help="Attr. energy prot-prot 1", default=0.00) 
-parser.add_argument("-EpsProt2", type=float, help="Attr. energy prot-prot 2", default=0.00)
-parser.add_argument("-EpsProt3", type=float, help="Attr. energy prot-prot 3", default=0.00)
+# > 0 for multilayer adsorption
+parser.add_argument("-EpsProt1", type=float, help="Attr. energy prot-prot 1", default=4.00) 
+parser.add_argument("-EpsProt2", type=float, help="Attr. energy prot-prot 2", default=3.75)
+parser.add_argument("-EpsProt3", type=float, help="Attr. energy prot-prot 3", default=3.50)
 
-# EpsNP_HSAwSiO2 = 10.8
-# EpsNP_TfwSiO2  = 8.9
-# EpsNP_FibwSiO2 = 20.
+''' Range of the Protein-protein interaction near the surface [nm] '''
+parser.add_argument("-K_pp", type=float, help="Prot-prot 3-body range",       default=66.0)
+
+''' Switch ON/OFF the diagonal part of the Prot-Prot interaction energy matrix. 1 = ON, 0 = OFF '''
+parser.add_argument("-EpsDiag", type=int, help="Diagonal part of Prot-prot 3-body ", default=1)
 
 ''' Electrical Double layer interaction in [Kb*T] '''
 parser.add_argument("-EpsEDL1", type=float, help="EDL energy prot-NP 1",      default=0.52)
@@ -84,20 +94,9 @@ parser.add_argument("-Zpot1", type=float, help="Z-potential prot 1",      defaul
 parser.add_argument("-Zpot2", type=float, help="Z-potential prot 2",      default=-20.)
 parser.add_argument("-Zpot3", type=float, help="Z-potential prot 3",      default=-15.)
 
-# C_HSAwSiO2(half-coverage) = 0.175
-# C_TfwSiO2(half-coverage)  = 0.055
-# C_FibwSiO2(half-coverage) = ???
-
-''' Reference starting concentrations [mg/ml] '''
-parser.add_argument("-C_NP",    type=float, help="Conc. of NPs",              default=0.100)
-parser.add_argument("-C_Prot1", type=float, help="Conc. of Protein 1",        default=0.005)
-parser.add_argument("-C_Prot2", type=float, help="Conc. of Protein 2",        default=0.350)
-parser.add_argument("-C_Prot3", type=float, help="Conc. of Protein 3",        default=0.350)
-
-# density of the NP in g/cm3 (COULD BE SET AS AN ARGUMENT)
-rho_SiO2 = 2.65  
-
 args, unknown = parser.parse_known_args()
+
+
 
 gamma = [0]*3
 
@@ -111,9 +110,13 @@ gamma[2] = math.tanh(args.Zpot3/(4*args.kT*25.6))
 R1 = args.RNP
 
 V_NP = 4./3*3.1416*math.pow(R1*1E-7, 3) # cm3
-M_NP = rho_SiO2 * V_NP                  # g/cm3 * cm3 = grams
+M_NP = args.rho_NP * V_NP                  # g/cm3 * cm3 = grams
 
-print "Mass of the NP: ", M_NP
+''' mass conversion factor '''
+kDa_2_mg =  1.6601E-18
+mg_2_kDa =  1./1.6601E-18
+
+print "Mass of the NP: %1.1e kDa" % (M_NP*1000 * mg_2_kDa)
 
 C_NP = args.C_NP # mg/ml = g/l
 
@@ -123,7 +126,7 @@ V_box_1NP *= pow(1E9,3)      # nm3
 
 L_box_1NP = pow(V_box_1NP, 1./3) # nm
 
-print "Lenght of the box: ", L_box_1NP
+print "Lenght of the experimental box: %1.1f nm" % L_box_1NP
 
 
 ''' Nombre total (final) de molecules '''
@@ -152,7 +155,6 @@ for i in range(3):
 L = args.L
 V = L*L*L
 den0 = 1./V
-print("Total volume of the box %1.6lf" % V)
 
 C_Prot = [args.C_Prot1, args.C_Prot2, args.C_Prot3]   # mg/ml = g/l
 
@@ -173,7 +175,7 @@ N_Ref[0] = int(C_Prot[0] / M[0] / ml_2_nm3 * V_box_1NP )
 N_Ref[1] = int(C_Prot[1] / M[1] / ml_2_nm3 * V_box_1NP )
 N_Ref[2] = int(C_Prot[2] / M[2] / ml_2_nm3 * V_box_1NP )
 
-print "Number of proteins per NP: "
+print "[Exp] Number of proteins/NP: "
 print N_Ref
 
 N_min = [0]*3
@@ -182,8 +184,31 @@ N_min[0] = int(N_Ref[0] * V / V_box_1NP)
 N_min[1] = int(N_Ref[1] * V / V_box_1NP)
 N_min[2] = int(N_Ref[2] * V / V_box_1NP)
 
-print "Minimum number of proteins in simulation box: "
+print "[Sim] Minimum number of 'free' proteins in simulation box: "
 print N_min
+
+
+normFactor = 0.
+if N_min[0] > 0:
+	normFactor += N_min[0] + args.Nmin_Prot1
+if N_min[1] > 0:
+	normFactor += N_min[1] + args.Nmin_Prot2
+if N_min[2] > 0:
+	normFactor += N_min[2] + args.Nmin_Prot3
+
+
+''' Fraction of each protein From less to most abundant '''
+if N_min[0] > 0:
+	P1 = (N_min[0] + args.Nmin_Prot1)*1./normFactor
+else:
+	P1 = 0.
+if N_min[1] > 0:
+	P2 = (N_min[1] + args.Nmin_Prot2)*1./normFactor
+else:
+	P2 = 0.
+P3 = 1 - (P1 + P2)
+
+print "[Sim] Proportions of each protein:\n %1.2f %1.2f %1.2f" % (P1, P2, P3)
 
 ''' restore the masses to the original units '''
 M  = [args.M1,  args.M2,  args.M3]   # Massa de les molecules '''
@@ -192,9 +217,9 @@ Lreaction = L*0.8
 Lmin = (L - Lreaction)/2
 Lmax = L - Lmin
 
-print "Simulation box geometry"
-print "Lbox ", L, " Lreaction ", Lreaction, " boundaries ", Lmin, Lmax
-print("Volume: %1.3e L" % (V * (1e-8)**3))
+print "[Sim] Box geometry:"
+print "Lbox ", L, "nm Lreaction ", Lreaction, "nm boundaries ", Lmin, Lmax
+#print("Volume: %1.3e L" % (V * (1e-8)**3))
 
 Rmax = 0.
 for r in R:
@@ -281,12 +306,12 @@ while 1:
 	if(N == N0):
 		break
 
-print nReaction, nReactionRef
+#print nReaction, nReactionRef
 
-print("Number of accepted molecules: ",N)
+print "[Sim] Number of accepted molecules: ", N
 
 f1 = open(fileName,'w')                     # arxiu de sortida: init.dat
-f1.write('%d\t%f\t%f\t%f\t%f\t%d\t%d\t%d\n' % (N+1,args.C_PBS,L,V, L_box_1NP, N_Ref[0], N_Ref[1], N_Ref[2])) # capc,alera de l'arxiu
+f1.write('%d\t%f\t%f\t%d\t%f\t%f\t%f\t%d\t%d\t%d\t\n' % (N+1, args.C_PBS, args.K_pp, args.EpsDiag, L, V, L_box_1NP, N_Ref[0], N_Ref[1], N_Ref[2])) # capc,alera de l'arxiu
 
 
 # escriptura a arxiu amb les caracteristiques i coordenades de cada proteina
@@ -303,7 +328,7 @@ for i in range(nTypes):
 	conc = N_Ref[i]/V_box_1NP           # nm-3
 	
 	print("%d\tmolecules of type %d with concentration %1.5e nm-3" % (N,i,conc))
-	print("%d\tmolecules of type %d with concentration %1.5e M=1mol/L" % (N,i,conc/0.6))
+	print("%d\tmolecules of type %d with concentration %1.5e M=1mol/L\n" % (N,i,conc/0.6))
 
 	for coords in molType[i].molList:
 		f1.write('%d' % i)
