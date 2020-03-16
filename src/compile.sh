00926e29d9668a4644086a0a5e6b6955cae2e80b
ovilanova <oriol.vilanova@gmail.com>
2020-03-16 10:15:38 +0100

    Update compile.sh

--- src/compile.sh
+++ src/compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-nvcc -arch=sm_20 -use_fast_math -O2 -lcurand --ptxas-options -v mdgpu_np.cu -o mdgpu_np
+nvcc -D_FORCE_INLINES -arch=sm_20 -use_fast_math -O2 -lcurand --ptxas-options -v mdgpu_np.cu -o mdgpu_np
 
 cp mdgpu_np ..
