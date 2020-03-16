e2c71fe58e4cc2c6c58996359b1e7bdd6e6915d3
ovilanova <oriol.vilanova@gmail.com>
2020-03-16 16:48:40 +0100

    Update compile.sh

--- src/compile.sh
+++ src/compile.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-nvcc -D_FORCE_INLINES -arch=sm_20 -use_fast_math -O2 -lcurand --ptxas-options -v mdgpu_np.cu -o mdgpu_np
+# The -DSC flag switches the Soft-Corona interactions, remove it from the command to compile without these interactions
+
+nvcc -DSC -D_FORCE_INLINES -arch=sm_30 -use_fast_math -O2 -lcurand --ptxas-options -v mdgpu_np.cu -o mdgpu_np
 
 cp mdgpu_np ..
