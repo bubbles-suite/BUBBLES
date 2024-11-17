a1b35a71eed16c636d3d0a08267ac162b7eddc88
ovilanova <oriol.vilanova@gmail.com>
2024-11-17 17:23:44 +0100

    Update compile.sh
    Code updated for CUDA 12 compatibility

--- src/compile.sh
+++ src/compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# The -DSC flag switches the Soft-Corona interactions, remove it from the command to compile without these interactions
+# The -DSC flag switches the Soft-Corona interactions ON, remove it from the command to compile without these interactions (to set them OFF)
 
-nvcc -DSC -D_FORCE_INLINES -arch=sm_30 -use_fast_math -O2 -lcurand --ptxas-options -v mdgpu_np.cu -o mdgpu_np
+nvcc -DSC -D_FORCE_INLINES -use_fast_math -O2 -lcurand --ptxas-options -v mdgpu_np.cu -o mdgpu_np
 
 cp mdgpu_np ..
